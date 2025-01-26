c3be0791346ab0ecbe25c99ad807b0267e9988d0
braykoff <99614905+Braykoff@users.noreply.github.com>
2025-01-25 21:17:43 -0500

    Added timer to robot container init

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -10,6 +10,7 @@ import edu.wpi.first.networktables.NetworkTableInstance;
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.TimedRobot;
+import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.util.WPILibVersion;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -44,7 +45,9 @@ public final class Robot extends TimedRobot {
     canLogger.start();
 
     // Init robot base
+    double start = Timer.getFPGATimestamp();
     m_robotContainer = new RobotContainer();
+    System.out.printf("RobotContainer instantiation took %f seconds\n", Timer.getFPGATimestamp() - start);
 
     // Add logging periodic
     addPeriodic(m_robotContainer::log, 0.1, 0.001); // every 100 ms
