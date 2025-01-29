f49e9017513f371dba1729922acbf0f877786615
Alex Musiitwa <alexmusiitwa@gmail.com>
2025-01-28 20:08:09 -0400

    completion of elevator.java with sysID

--- src/main/java/frc/robot/subsystems/Elevator.java
+++ src/main/java/frc/robot/subsystems/Elevator.java
@@ -7,18 +7,8 @@ import com.ctre.phoenix6.StatusSignal;
 import com.ctre.phoenix6.controls.Follower;
 import com.ctre.phoenix6.controls.PositionVoltage;
 import com.ctre.phoenix6.hardware.TalonFX;
-import com.revrobotics.AbsoluteEncoder;
-import com.revrobotics.RelativeEncoder;
-import com.revrobotics.spark.SparkClosedLoopController;
-import com.revrobotics.spark.SparkFlex;
-import com.revrobotics.spark.SparkBase.ControlType;
-import com.revrobotics.spark.SparkBase.PersistMode;
-import com.revrobotics.spark.SparkBase.ResetMode;
-import com.revrobotics.spark.SparkLowLevel.MotorType;
-
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.Voltage;
-import edu.wpi.first.util.datalog.DataLogEntry;
 import edu.wpi.first.util.datalog.DoubleLogEntry;
 import edu.wpi.first.wpilibj.Alert;
 import edu.wpi.first.wpilibj.DataLogManager;
@@ -26,29 +16,31 @@ import edu.wpi.first.wpilibj.sysid.SysIdRoutineLog.State;
 import edu.wpi.first.wpilibj.Alert.AlertType;
 import edu.wpi.first.wpilibj2.command.Subsystem;
 import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine;
-import edu.wpi.first.wpilibj2.command.sysid.SysIdRoutine.Config;
 import frc.robot.Constants.ElevatorConstants;
 import frc.robot.Constants.HardwareConstants;
 
 public class Elevator implements Subsystem {
   private final TalonFX elevatorMain = new TalonFX(HardwareConstants.ELEVATOR_MAIN_CAN, HardwareConstants.CANIVORE);
   private final TalonFX elevatorFollower = new TalonFX(HardwareConstants.ELEVATOR_FOLLOWER_CAN, HardwareConstants.CANIVORE);
-  private final SparkFlex manipulatorPivot = new SparkFlex(HardwareConstants.MANIPULATOR_PIVOT_CAN, MotorType.kBrushless);
-
-  public SparkClosedLoopController manipulatorPivotController = manipulatorPivot.getClosedLoopController();
-  public RelativeEncoder manipulatorPivotEncoder = manipulatorPivot.getEncoder();
-  public AbsoluteEncoder manipulatorPivotAbsoluteEncoder = manipulatorPivot.getAbsoluteEncoder();
+  private final TalonFX manipulatorPivot = new TalonFX(HardwareConstants.MANIPULATOR_PIVOT_CAN, HardwareConstants.RIO_CAN);
 
   private final PositionVoltage elevatorPositionRequest = new PositionVoltage(.0)
     .withOverrideBrakeDurNeutral(false)
     .withSlot(0);
+  
+  private final PositionVoltage manipulatorPivotPositionRequest = new PositionVoltage(0)
+    .withOverrideBrakeDurNeutral(true)
+    .withSlot(0);
 
   private final StatusSignal<Angle> elevatorPosition = elevatorMain.getPosition();
+  private final StatusSignal<Angle> manipulatorPosition = manipulatorPivot.getPosition();
   
-  private final Alert elevatorAlert = new Alert("Elevator Alert", AlertType.kError);
-  private final Alert manipulatorAlert = new Alert("Manipulator Alert", AlertType.kError);
+  private final Alert elevatorMainAlert = new Alert("Elevator Main Motor Alert", AlertType.kError);
+  private final Alert elevatorFollowerAlert = new Alert("Elevator Follower Motor Alert", AlertType.kError);
+  private final Alert manipulatorAlert = new Alert("Manipulator pivot motor alert", AlertType.kError);
 
   private SysIdRoutine elevatorRoutine, manipulatorPivotRoutine;
+
   //DataLog
   private final DoubleLogEntry elevatorPositionLog = 
     new DoubleLogEntry(DataLogManager.getLog(), "Elevator/Position", "rotations");
@@ -65,13 +57,11 @@ public class Elevator implements Subsystem {
 
     elevatorFollower.setControl(new Follower(HardwareConstants.ELEVATOR_MAIN_CAN, false));
 
-    manipulatorPivot.configure(ElevatorConstants.MANIPULATOR_PIVOT_CONFIG,
-     ResetMode.kResetSafeParameters,
-     PersistMode.kPersistParameters);
+    manipulatorPivot.getConfigurator().apply(ElevatorConstants.MANIPULATOR_PIVOT_CONFIG);
   }
 
   /**
-   * @param rotations sets elevator to set rotations (in rots)
+   * @param rotations sets elevator to set rotations (in rots). 0.0 is bottom.
    */
   public void setElevatorPosition(double rotations){
     elevatorMain.setControl(elevatorPositionRequest.withPosition(rotations));
@@ -79,13 +69,12 @@ public class Elevator implements Subsystem {
   }  
   
   /**
-   * @param rotations sets Manipulator to position (in rotations)
+   * @param rotations sets Manipulator to position (in rotations). 0.0 is horizontal.
    */
   public void setManipulatorPivotPosition(double rotations) {
-    manipulatorPivotController.setReference(rotations, ControlType.kPosition);
+    manipulatorPivot.setControl(manipulatorPivotPositionRequest.withPosition(rotations));
     manipulatorPivotSetPointLog.append(rotations);
   }
-
 
   public SysIdRoutine getElevatorRoutine() {
     System.out.println("Starting CTRE SignalLogger due to getRoutine call in SwerveDrive");
@@ -113,8 +102,11 @@ public class Elevator implements Subsystem {
       manipulatorPivotRoutine = new SysIdRoutine(
         new SysIdRoutine.Config(null, null, null, (State state) -> {
           //Logging State
-          SignalLogger.writeString("manipulatorPivot-SysID-State", state.toString());
-        }), null);
+          SignalLogger.writeString("ManipulatorPivot-SysID-State", state.toString());
+        }), new SysIdRoutine.Mechanism((Voltage v) -> {
+          manipulatorPivot.setVoltage(v.in(Volts));
+        }, null, this)
+      );
     }
 
     return manipulatorPivotRoutine;
@@ -123,11 +115,12 @@ public class Elevator implements Subsystem {
 
   public void log() {
     elevatorPositionLog.append(elevatorPosition.getValueAsDouble());
-    manipulatorPivotPositionLog.append(manipulatorPivotAbsoluteEncoder.getPosition());
+    manipulatorPivotPositionLog.append(manipulatorPosition.getValueAsDouble());
   }
 
   public void checkHardware(){
-    elevatorAlert.set(!elevatorMain.isAlive() || !elevatorMain.isConnected());
-    manipulatorAlert.set(manipulatorPivot.hasActiveFault() || manipulatorPivot.hasActiveWarning());
+    elevatorMainAlert.set(!elevatorMain.isAlive() || !elevatorMain.isConnected());
+    elevatorFollowerAlert.set(!elevatorFollower.isAlive() || !elevatorFollower.isConnected());
+    manipulatorAlert.set(!manipulatorPivot.isAlive() || !manipulatorPivot.isConnected());
   }
 }
